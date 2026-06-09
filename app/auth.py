16a3c22c8112e7f622fd119446ce1089332cbb94
sujeet hole <sujeethole70@gmail.com>
2026-06-09 21:36:05 +0530

    Remove locust import from auth

--- app/auth.py
+++ app/auth.py
@@ -1,20 +1,30 @@
-from locust import HttpUser, task
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
-class SocialMediaUser(HttpUser):
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-    @task(5)
-    def get_feed(self):
-        self.client.get("/feed")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    @task(3)
-    def get_posts(self):
-        self.client.get("/posts")
 
-    @task(1)
-    def create_post(self):
-        self.client.post(
-            "/posts",
-            json={
-                "content": "Locust testing"
-            }
-        )
+def create_jwt_token(data: dict):
+    payload = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload["exp"] = expire
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
